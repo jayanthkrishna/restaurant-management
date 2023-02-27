e9dab2be57bbc5b147d7aef21fdc29d4c23c7de4
jayanthkrishna <gannerla16038@iiitd.ac.in>
2023-02-27 23:55:08 +0530

    working order controllers

--- controllers/orderContrller.go
+++ controllers/orderContrller.go
@@ -1,9 +1,35 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jayanthkrishna/restaurant-management-golang/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		res, err := orderCollection.Find(ctx, bson.M{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while ordering items"})
+		}
+		var allOrders []bson.M
+
+		if err = res.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allOrders)
 
 	}
 }
